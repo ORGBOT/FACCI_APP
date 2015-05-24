4c6ef4419cdc39dd556b91cc8fc6372d5d2df533
JULIO BARBERAN <juliobarberan@Mac-Pro-de-JULIO.local>
2015-05-24 16:38:01 -0500

    FIRTS COMMIT

--- MAPS/ViewController.swift
+++ MAPS/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import MapKit
+class ViewController: UIViewController,MKMapViewDelegate  {
 
-class ViewController: UIViewController {
-
+    
+    @IBOutlet var theMapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        var latitude:CLLocationDegrees = 48.399193
+        var longitude:CLLocationDegrees = 9.993341
+        var latDelta:CLLocationDegrees = 0.01
+        var longDelta:CLLocationDegrees = 0.01
+        
+        var theSpan:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        var churchLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        var theRegion:MKCoordinateRegion =  MKCoordinateRegionMake(churchLocation, theSpan)
+        self.theMapView.setRegion(theRegion, animated: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
